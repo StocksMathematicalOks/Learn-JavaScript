05254c4460a1f9cc9681376e9ccdea9d747cb431
StocksMathematicalOks <88820859+StocksMathematicalOks@users.noreply.github.com>
2021-08-22 10:15:18 +0100

    Create RegexBasics.js

---
+++ RegexBasics.js
@@ -0,0 +1,5 @@
+    /*Regex Global Case*/: g /*(Check for all values in a string)*/
+    /*Regex Insensitive Case*/: i /*(Caps do not matter)*/
+    /*Regex Alphabet*/: /[a-z]/ /*or*/ /[a-z]/gi
+    /*Regex Numbers:*/ /0-9/ or /0-9/gi
+    /*Regex Not-A Match:*/ ^ (Example: /^[a-z]/)
